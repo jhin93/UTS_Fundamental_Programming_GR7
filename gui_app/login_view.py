ebd28f46bb70ab2455e92ba793c1cfe148359b7d
HARSHIT2780 <84771111+HARSHIT2780@users.noreply.github.com>
2025-05-19 19:13:16 +1000

    Add files via upload

---
+++ gui_app/login_view.py
@@ -0,0 +1,30 @@
+import tkinter as tkt
+from tkinter import messagebox
+from gui_controller import handle_login
+from exception_view import show_exception
+
+
+class LoginView:
+    def __init__(self, window):
+        self.window = window
+        self.window.title(" main Login Window")
+
+        tkt.Label(window, text="Email id:").pack()
+        self.email_entry = tkt.Entry(window)
+        self.email_entry.pack()
+
+        tkt.Label(window, text="Password:").pack()
+        self.password_entry = tkt.Entry(window, show="*")
+        self.password_entry.pack()
+
+        tkt.Button(window, text="Login", command=self.login).pack(pady=10)
+
+    def login(self):
+        email = self.email_entry.get()
+        password = self.password_entry.get()
+        success, message = handle_login(email, password)
+
+        if success:
+            messagebox.showinfo("Successfull", message)
+        else:
+            show_exception(message)
